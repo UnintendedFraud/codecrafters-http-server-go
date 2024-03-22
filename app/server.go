d0a747b49f4addfd454400e06825bf48db07ea8b
unintendedfraud <briceunintendedfraud@fastmail.com>
2024-03-22 18:26:28 +0100

    extract path from request

--- app/server.go
+++ app/server.go
@@ -4,26 +4,70 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+type RequestData struct {
+	path string
+}
 
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	con, err := l.Accept()
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(con)
+	}
+}
+
+func handleConnection(con net.Conn) {
+	defer func() {
+		if err := con.Close(); err != nil {
+			fmt.Println("Error closing the connection: ", err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	data, err := getRequestData(con)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error reading the request data: ", err.Error())
 		os.Exit(1)
 	}
-	defer con.Close()
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
-	con.Write([]byte(response))
+	con.Write([]byte(getResponse(data.path)))
+}
 
+func getRequestData(con net.Conn) (RequestData, error) {
+	b := make([]byte, 256)
+
+	_, err := con.Read(b)
+	if err != nil {
+		return RequestData{}, err
+	}
+
+	reqData := string(b)
+	data := strings.Split(reqData, "\r\n")
+
+	path := strings.Split(data[0], " ")[1]
+
+	return RequestData{
+		path,
+	}, nil
+}
+
+func getResponse(path string) string {
+	if path == "/" {
+		return "HTTP/1.1 200 OK\r\n\r\n"
+	}
+
+	return "HTTP/1.1 404 Not Found\r\n\r\n"
 }
